8812d74495f7dd314d547ec03da452b044cf8539
Ksenia <kseniarassadina@gmail.com>
2021-10-08 20:49:35 +0300

    added catalog,login,news test test  for mainpage

--- src/test/java/test_project/tests/MainPageTests.java
+++ src/test/java/test_project/tests/MainPageTests.java
@@ -1,11 +1,12 @@
 package test_project.tests;
 
 
+import org.openqa.selenium.By;
 import test_project.helpers.DriverUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.files.DownloadActions.click;
@@ -28,61 +29,74 @@ public class MainPageTests extends TestBase {
         });
     }
 
-
     @Test
-    @DisplayName("Перейти на страницу Каталога")
-    void testCatalog(){
+    @DisplayName("Вход/Регистрация")
+    void loginTest() {
         step("Open url 'https://www.vprok.ru/'", () ->
-                open("https://www.vprok.ru/"));
-        step("Перейти на страницу Каталога", () -> {
-            $(".xfnew-header__catalog-button").click();
-            $$(".fo-catalog-menu").findBy(text("Молоко, сыр, яйца")).click();
+             open("https://www.vprok.ru/"));
+             $(".xfnew-header__user-nav:nth-child(5)").click();
+
+        step("Проверяем, что открылся попап авторизации", () -> {
+             $(".xf-auth-title__text")
+                        .shouldHave(text("Вход или регистрация"));
         });
     }
 
     @Test
-    @DisplayName("Сменить регион")
-    void testChangeRegion(){
+    @DisplayName("Попап регионов")
+    void regionTest() {
         step("Open url 'https://www.vprok.ru/'", () ->
-                open("https://www.vprok.ru/"));
-        step("Регион", () -> {
+            open("https://www.vprok.ru/"));
             $(".xfnew-header__change-region").click();
-            $(".xf-popup-polygons__block-check-region").shouldHave(text("Выберите ваш регион"));
+
+        step("Проверяем, что открылся попап с регионами", () -> {
+            $(".xf-popup-polygons__block-check-region")
+                    .shouldHave(text("Выберите ваш регион"));
         });
     }
 
     @Test
     @DisplayName("Открыть страницу с акциями")
-    void testOpenPromoPage(){
-        step("Open url 'https://www.vprok.ru/'", () ->
+    void promoTest() {
+        step("Кликнуть на блок акции в шапке", () ->
                 open("https://www.vprok.ru/"));
-       step("Кликнуть на блок Акции над шапкой", () -> {
-            $(byText("Акции")).click();;
-           $(".xf-caption__title").shouldHave(text("Акции"));
+                $(byText("Акции")).click();;
+
+        step("Проверяем, что открылась страница с акциями", () -> {
+                $(".xf-caption__title")
+                        .shouldHave(text("Акции"));
         });
-   }
-
-
-
-
-
-    @Test
-    @DisplayName("Открыть рецепт")
-    void testOpenRecipe(){
-        step("Open url 'https://www.vprok.ru/'", () ->
-                open("https://www.vprok.ru/"));
-        step("Проскролить до блока с рецептами", () -> {
-            $(".xf-mp-recipe-news__list").scrollIntoView(true);
-
-
-        });
-//        step("Проверяем, что открылась страница с рецептом", () -> {
-//            $(".xf-popup-polygons__block-check-region").shouldHave(text("Выберите ваш регион"));
-//        });
     }
 
 
+    @Test
+    @DisplayName("Открыть раздел Каталога")
+    void testCatalog(){
+        step("Open url 'https://www.vprok.ru/'", () ->
+                open("https://www.vprok.ru/"));
+                $(".xfnew-header__catalog-button").click();
+                $$(".fo-catalog-menu__nav a").
+                        findBy(text("Молоко, сыр, яйца")).click();
 
+        step("Проверяем, что открылась страница каталога", () -> {
+                $(".fo-breadcrumbs__current").
+                        shouldHave(text("Молоко, сыр, яйца"));;
+        });
+    }
+
+
+    @Test
+    @DisplayName("Открыть новость")
+    void newsTest() {
+        step("Кликнуть на блок акции в шапке", () ->
+                open("https://www.vprok.ru/"));
+                $(".xf-mp-recipe-news__list").scrollIntoView(true).click();
+                $(".xf-mp-recipe-news__img-block").click();
+
+        step("Проверяем, что открылась страница с новостью", () -> {
+                $(".xf-caption__title").shouldBe(visible);
+        });
+    }
 
 
     @Test
